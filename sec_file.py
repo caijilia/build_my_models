44524a48c5f7f8ac06fd7c09e49a3bd1d7627847
caijilia <87253394+caijilia@users.noreply.github.com>
2023-07-28 17:23:17 +0800

    Update sec_file.py

--- sec_file.py
+++ sec_file.py
@@ -1,7 +1,14 @@
 import torch
-import numpy
+import numpy as np
 import sklearn
 import cv2
 
-print(numpy.__version__)
+print(np.__version__)
 print(torch.__version__)
+
+class unet:
+  def __init__(self, chnnels=6):
+    self.ch = channels
+  def forward(self, x):
+    x1 = self.ch + 256
+    return x1
