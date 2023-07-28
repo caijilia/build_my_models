0922bac7b4caa867a86105082ab0f1d6f8b69ed5
hao03.li <hao03.li@partner.samsung.com>
2023-07-28 09:49:34 +0000

    build res_unet

--- sec_file.py
+++ sec_file.py
@@ -12,3 +12,11 @@ class unet:
     def forward(self, x):
         x1 = self.ch + 256
         return x1
+
+class res_unet:
+    def __init__(self, chnnels=6, weight = 0.5):
+        self.ch = chnnels
+        self.weight = weight
+    def forward(self, x):
+        x1 = self.ch + 256 + (self.ch * weight)
+        return x1
